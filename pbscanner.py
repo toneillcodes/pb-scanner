1edfbd91f494f545c0ffa9cbb414c05fa7173366
toneillcodes <148013535+toneillcodes@users.noreply.github.com>
2024-08-28 22:04:18 -0400

    Create pbscanner.py

---
+++ pbscanner.py
@@ -0,0 +1,68 @@
+import sys
+import requests
+import json
+import time
+import re
+
+pbRoot = input('Enter the base URL for PB (ex: https://school.edu/BannerExtensibility): ');
+pbPageUrl = '/internalPb/virtualDomains.pbadmListPages';
+nonPbPageUrl = '/internalPb/virtualDomains.pbadmNonAdmPages';
+pageRoot = '/customPage/page/';
+
+accessiblePages = list();
+inaccessiblePages = list();
+
+print("Starting scan of host " + pbRoot);
+
+## test the root
+pbPageList = requests.get(pbRoot + pbPageUrl);
+pbPageListJson = pbPageList.json();
+
+pageCount = len(pbPageListJson);
+print("DEBUG: pageCount: " + str(pageCount));
+
+for page in pbPageListJson:
+    pageName = page['CONSTANT_NAME'];
+    pageUrl = pbRoot + pageRoot + pageName;
+    #print("DEBUG: url: " + pageUrl);
+    pageTest = requests.get(pageUrl, allow_redirects=False);
+    #print("DEBUG: pageTest.status_code: " + str(pageTest.status_code));
+    if pageTest.status_code == 200:
+        #print("page (" + pageName + ") is accessible");
+        print('.', end='', flush=True)
+        # let's check the response for domains: pbResource('virtualDomains.exampleDomain');
+        # regexp = "pbResource\(\'(.*)\'\)"gm
+        domainList = re.findall("pbResource\(\'(.*)\'\)", pageTest.text);
+        if(domainList):
+            #print("DEBUG: pageName: " + " domainList: " + str(domainList));
+            testdict = { 'name': pageName, 'url': pageUrl, 'domains': str(domainList) };
+            ##  foreach domain we should check the next instance of queryParams
+            ##  queryParams: '{p_id : $scope.BlockSearchStudent_InputID}',
+            ##  /queryParams: '{(.*)}'/gm
+        else:
+            testdict = { 'name': pageName, 'url': pageUrl, 'domains': 'none' };
+        accessiblePages.append(testdict);
+    else:
+        #print("page (" + pageName + ") is NOT accessible");
+        print('.', end='', flush=True)
+        inaccessiblePages.append(pageUrl);
+    #time.sleep(1)
+
+print("");  # empty line for formatting
+print("----------------------------------");
+print("Summary Report");
+accessibleCount = len(accessiblePages);
+print("Accessible count: " + str(accessibleCount));
+
+percentageAccessible = (accessibleCount / pageCount)*100;
+print("Percentage accessible = " + str(percentageAccessible));
+print("----------------------------------");
+
+listPages = input("List accessible pages? (Y/N) ");
+
+if(listPages == "Y"):
+    print("Listing accessible pages, URLs and associated domains:");
+    for page in accessiblePages:
+      print(page);
+      #print("domains for " + x['name']);
+      #print(x['name']);
